260f608e628a0bcc9cae49745ed03f79a9c5a6cf
Felix Bauer <felix.bauer@stud.hawk.de>
2022-06-27 13:08:48 +0200

    Created new Player-Class

---
+++ Uno/Classes/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno.Classes
+{
+    public class Player
+    {
+
+        public Player(string name, string ip_port)
+        {
+            Name = name;
+            this.ip_port = ip_port;
+        }
+        public Player() : this("?", "?") { }
+
+        
+        public string Name { get; set; }
+        public string ip_port { get; set; }
+    }
+}
