82e247ffb0c532ee594c0de6295b502599b8def7
Felix Bauer <felix.bauer@stud.hawk.de>
2022-06-28 11:04:03 +0200

    Added global Player-List with add and remove Methods

--- Uno/Classes/Globals.cs
+++ Uno/Classes/Globals.cs
@@ -11,5 +11,27 @@ namespace Uno.Classes
         public static Networking network = new Networking();
         public static List<Player> Players;
         public static string myName;
+
+        static Globals()
+        {
+            Players = new List<Player>();
+        }
+
+        public static void addPlayer(string name, string IpPort)
+        {
+            Players.Add(new Player(name, IpPort));
+        }
+
+        public static void removePlayer(string IpPort)
+        {
+            foreach (Player player in Players)
+            {
+                if(player.ip_port == IpPort)
+                {
+                    Players.Remove(player);
+                    return;
+                }
+            }
+        }
     }
 }
