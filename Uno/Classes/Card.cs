0c339d708d5ec19431baf7f1ba5bf2d44c6a5e1a
Felix Bauer <felix.bauer@stud.hawk.de>
2022-06-30 12:17:26 +0200

    New Card class added

---
+++ Uno/Classes/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno.Classes
+{
+    /*
+     Only standard Cards, no specials or exceptions
+     */
+    internal class Card
+    {
+        public uint number;
+        public string color;
+
+        public Card(uint number, string color)
+        {
+            this.number = number;
+            this.color = color;
+        }
+    }
+
+}
