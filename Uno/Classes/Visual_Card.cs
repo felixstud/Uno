8162c66883489184c99389bbd67d8bcdb5db4ac8
Felix Bauer <felix.bauer@stud.hawk.de>
2022-07-07 10:02:44 +0200

    Added Class for Card Layout (Label) not working

---
+++ Uno/Classes/Visual_Card.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Uno.Classes
+{
+    public class Visual_Card
+    {
+        public Label card;
+
+        public Visual_Card(Card data)
+        {
+            var colors = new[] {
+            Brushes.Red,
+            Brushes.Green,
+            Brushes.Blue,
+            Brushes.Yellow
+            };
+            card = new Label();
+            card.Width = 30;
+            card.Height = 70;
+            card.FontSize = 20;
+            card.Visibility = System.Windows.Visibility.Visible;
+            card.VerticalContentAlignment = System.Windows.VerticalAlignment.Center;
+            card.HorizontalContentAlignment = System.Windows.HorizontalAlignment.Center;
+            card.Background = colors[data.color];
+            card.Content = data.number.ToString();
+        }
+    }
+}
