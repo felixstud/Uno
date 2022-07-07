91bbaa66ae29e3abd47c6be8fe7e08e0399d944b
Felix Bauer <felix.bauer@stud.hawk.de>
2022-07-07 10:01:02 +0200

    Card Class added

---
+++ Uno/Classes/Card.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno.Classes
+{
+    public class CardStack
+    {
+        public List<Card> Cards { get; set; }
+
+        public CardStack()
+        {
+            this.Cards = new List<Card>();
+        }
+
+        public void createAllCards()
+        {
+            /*
+             * Every Card twice except for number 0
+             */
+            for(int i = 0; i < 4; i++)  //4 Colors
+            {
+                for (int k = 0; k < 2; k++)
+                {
+                    for (int j = 1; j <= 9; j++) //Numbers
+                    {
+                        this.Cards.Add(new Card(j, i));
+                    }
+                }
+                this.Cards.Add(new Card(0, i));      
+            }
+        }
+
+        public Card getRandomCard()
+        {
+            int rnd = Globals.randomNumber.Next(0, this.Cards.Count() - 1);
+            Card ret = Cards[rnd];
+            Cards.Remove(Cards[rnd]);
+            return ret;
+        }
+
+        public void AddCard(Card add)
+        {
+            Cards.Add(add);
+        }
+
+        public Card RemoveCard(Card rem)
+        {
+            if (!Cards.Contains(rem))
+                return null;
+            Card c = Cards[Cards.IndexOf(rem)];
+            Cards.Remove(rem);
+            return c;
+        }
+
+        public int getCounter()
+        {
+            return Cards.Count();
+        }
+
+        public Card returnCard(int index)
+        {
+            if (index < Cards.Count)
+                return Cards[index];
+            return null;
+        }
+    }
+    
+    public class Card
+    {
+        public int number;
+        public int color;
+
+        public Card(int number, int color)
+        {
+            this.number = number;
+            this.color = color;
+        }
+    }
+}
